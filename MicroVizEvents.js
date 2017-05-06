8071a79b359cfb8b8d1e5d9ae64c1622f78de906
Alessandro Warth <alexwarth@gmail.com>
2017-05-06 14:06:45 -0700

    bug fix

--- MicroVizEvents.js
+++ MicroVizEvents.js
@@ -12,25 +12,46 @@ class MicroVizEvents {
   }
 
   add(event) {
+
+    // (1) do nothing -- no overlap
+    //   x = 1; y = 2;         [x = 1] [y = 2]
+
+    // (2) do nothing -- inside-out rule
+    //   var x = f();          [f()] [x = 5]
+
+    // (2.5) introduce new local event group
+    //   var x = f(            [f()  [x = 5]
+    //            );            ...]
+
+    // (3) introduce new local event group -- pre-events
+    //   for f()               [f()] [for
+    //   to: g()               [g()]  ...
+    //   do: {...}                    ...]
+
+    // (4) local event groups (2nd and 3rd are new iterations)
+    //   var sum = 0;          [sum = 0]
+    //   for 1 to: 3 do: {x|   [x = 1]   | [x = 2]   | [x = 3]
+    //     sum = sum + x;      [sum = 1] | [sum = 3] | [sum = 6]
+    //   };                              |           |
+
     const eventIsLocal = this.sourceLoc.contains(event.sourceLoc);
-    if (this.isLocal(event) &&
-        (!this.lastEventGroup ||
-         !(this.lastEventGroup instanceof LocalEventGroup) ||
-         event.sourceLoc.startPos < this.lastEventGroup.lastEvent.sourceLoc.startPos)) {
-      const isNewIteration =
-          this.lastEventGroup instanceof RemoteEventGroup ||
-          this.lastEventGroup instanceof LocalEventGroup &&
-              !event.sourceLoc.contains(this.lastEventGroup.lastEvent.sourceLoc);
-      this.eventGroups.push(new LocalEventGroup(isNewIteration));
+    if (eventIsLocal && this.lastEventGroup instanceof LocalEventGroup) {
+      if (this.lastEventGroup instanceof LocalEventGroup) {
+        if (event.sourceLoc.startPos >= this.lastEventGroup.lastEvent.sourceLoc.endPos ||
+            event.sourceLoc.startLineNumber >= this.lastEventGroup.lastEvent.sourceLoc.endLineNumber) {
+          // no-op
+        } else {
+          const isNewIteration = !event.sourceLoc.contains(this.lastEventGroup.lastEvent.sourceLoc);
+          this.eventGroups.push(new LocalEventGroup(isNewIteration));
+        }
+      }
+    } else if (eventIsLocal && !(this.lastEventGroup instanceof LocalEventGroup)) {
+      this.eventGroups.push(new LocalEventGroup(false));  // not a new iteration
     } else if (!eventIsLocal && !(this.lastEventGroup instanceof RemoteEventGroup)) {
       const isNewIteration = !!this.lastEventGroup;
       this.eventGroups.push(new RemoteEventGroup(isNewIteration));
     }
     this.lastEventGroup.add(event);
-  }
-
-  isLocal(event) {
-    return this.sourceLoc.contains(event.sourceLoc);
   }
 }
 
