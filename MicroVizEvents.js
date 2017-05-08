15fbd51a49d9f2191fa201f275ec3fee1f3a6b57
Alessandro Warth <alexwarth@gmail.com>
2017-05-08 11:41:57 -0700

    moar betta

--- MicroVizEvents.js
+++ MicroVizEvents.js
@@ -12,36 +12,47 @@ class MicroVizEvents {
   }
 
   add(event) {
-
-    // (1) do nothing -- no overlap
-    //   x = 1; y = 2;         [x = 1] [y = 2]
-
-    // (2) do nothing -- inside-out rule
-    //   var x = f();          [f()] [x = 5]
-
-    // (2.5) introduce new local event group
-    //   var x = f(            [f()  [x = 5]
-    //            );            ...]
-
-    // (3) introduce new local event group -- pre-events
-    //   for f()               [f()] [for
-    //   to: g()               [g()]  ...
-    //   do: {...}                    ...]
-
-    // (4) local event groups (2nd and 3rd are new iterations)
-    //   var sum = 0;          [sum = 0]
-    //   for 1 to: 3 do: {x|   [x = 1]   | [x = 2]   | [x = 3]
-    //     sum = sum + x;      [sum = 1] | [sum = 3] | [sum = 6]
-    //   };                              |           |
-
     const eventIsLocal = this.sourceLoc.strictlyContains(event.sourceLoc);
     if (eventIsLocal && this.lastEventGroup instanceof LocalEventGroup) {
-      if (!event.sourceLoc.equals(this.lastEventGroup.lastEvent.sourceLoc) &&
-          (event.sourceLoc.startPos >= this.lastEventGroup.lastEvent.sourceLoc.endPos ||
-           event.sourceLoc.startLineNumber >= this.lastEventGroup.lastEvent.sourceLoc.endLineNumber)) {
+
+      // "Just accumulating events"
+      //   x = 1; y = 2;
+      // [y = 2] goes "forward in pos", so it can be placed into the same LEG.
+
+      // "Toby's rule"
+      //   var sum = 0;          [sum = 0]
+      //   for 1 to: 3 do: {x|   [x = 1]   | [x = 2]   | [x = 3]
+      //     sum = sum + x;      [sum = 1] | [sum = 3] | [sum = 6]
+      //   };                              |           |
+      // Each [x = _] after the first is going "back in pos" so it's the start of a new iteration.
+
+      // Another example of "Toby's rule"
+      //   [1,2,3].reduce((x, y) => x + y);
+      // Each x declaration after the first starts a new iteration.
+
+      // "Inside-out rule", case 1
+      //   var x = f();
+      // Event starts @ same line as nested event ends -- so no LEGs req'd.
+
+      // "Inside-out rule", case 2
+      //   var x = f(
+      //           );
+      // x's decl goes "back in pos" so it must go into a new LEG -- but not a new iteration.
+
+      // "Inside-out rule", case 2 (another example: "pre-events" for a send)
+      //   for f()
+      //   to: g()
+      //   do: {...}
+      // A new LEG -- but not a new iteration -- is needed for the for-to-do send.
+
+      const lastEvent = this.lastEventGroup.lastEvent;
+      const eventNestsLastEvent = event.sourceLoc.strictlyContains(lastEvent.sourceLoc);
+      if (event.sourceLoc.startPos >= this.lastEventGroup.lastEvent.sourceLoc.endPos ||
+          eventNestsLastEvent &&
+              event.sourceLoc.startLineNumber >= lastEvent.sourceLoc.endLineNumber) {
         // no-op
       } else {
-        const isNewIteration = !event.sourceLoc.strictlyContains(this.lastEventGroup.lastEvent.sourceLoc);
+        const isNewIteration = !eventNestsLastEvent;
         this.eventGroups.push(new LocalEventGroup(isNewIteration));
       }
     } else if (eventIsLocal && !(this.lastEventGroup instanceof LocalEventGroup)) {
